a19c84502bda9e5e6f9bc651db9d25fdaaa44d09
Bugra Veysel Yildiz <56548187+bugrayildiz1@users.noreply.github.com>
2020-12-10 01:08:33 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const navSlide = () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav-links');
-    const navLinks = document.querySelectorAll('.nav-links li');
+// const navSlide = () => {
+//     const burger = document.querySelector('.burger');
+//     const nav = document.querySelector('.nav-links');
+//     const navLinks = document.querySelectorAll('.nav-links li');
 
-    burger.addEventListener('click', () => {
-        nav.classList.toggle('nav-active');
+//     burger.addEventListener('click', () => {
+//         nav.classList.toggle('nav-active');
 
-        //Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation){
-                link.style.animation = '';
-            } else {
-                link.style.animation = 'navLinkFade 0,5s ease forwards $(index/7 + 1.5)s';
-            }
-        });
+//         //Animate Links
+//         navLinks.forEach((link, index) => {
+//             if (link.style.animation){
+//                 link.style.animation = '';
+//             } else {
+//                 link.style.animation = 'navLinkFade 0,5s ease forwards $(index/7 + 1.5)s';
+//             }
+//         });
 
-        burger.classList.toggle('toggle');
-    });
-}
+//         burger.classList.toggle('toggle');
+//     });
+// }
 
 
-navSlide();
+// navSlide();
 
